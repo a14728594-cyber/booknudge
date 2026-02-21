1333d352f77778e5f76f4570c428f86ce8f5e62b
Kawano Shingo <a14728594@gmail.com>
2026-02-21 19:19:10 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -47,6 +47,7 @@
  *
  * The mainPage value must match a key in the PAGES object exactly.
  */
+import adminbookeditBookid from './pages/AdminBookEdit[bookId]';
 import AdminBooks from './pages/AdminBooks';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminInquiries from './pages/AdminInquiries';
@@ -76,11 +77,11 @@ import Support from './pages/Support';
 import Terms from './pages/Terms';
 import Tokushoho from './pages/Tokushoho';
 import genreDomain from './pages/genre[domain]';
-import adminbookeditBookid from './pages/AdminBookEdit[bookId]';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
+    "AdminBookEdit[bookId]": adminbookeditBookid,
     "AdminBooks": AdminBooks,
     "AdminDashboard": AdminDashboard,
     "AdminInquiries": AdminInquiries,
@@ -110,7 +111,6 @@ export const PAGES = {
     "Terms": Terms,
     "Tokushoho": Tokushoho,
     "genre[domain]": genreDomain,
-    "AdminBookEdit[bookId]": adminbookeditBookid,
 }
 
 export const pagesConfig = {
